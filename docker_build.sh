b24e56638d7f9f16652000f36da0011fd9d08bc9
Stefan van der Walt <stefanv@berkeley.edu>
2017-04-26 17:24:15 -0700

    Use correct config file name for new cesium_web

--- docker_build.sh
+++ docker_build.sh
@@ -10,5 +10,5 @@ docker build -t cesium/web .
 echo "Sleeping for 60 seconds while server starts"
 sleep 60
 
-echo -e "server:\n  url: http://localhost:9000" > cesium-docker.yaml
+echo -e "server:\n  url: http://localhost:9000" > cesium.yaml
 PYTHONPATH=. xvfb-run -a pytest -v cesium_app/tests/frontend/test_pipeline_sequentially.py
