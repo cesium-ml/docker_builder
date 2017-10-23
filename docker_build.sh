17f845dc9d7d4db542b604e414c41cea1a466f0e
Brett Naul <brettnaul@gmail.com>
2017-10-23 09:22:52 -0700

    Explicitly install baselayer requirements

--- docker_build.sh
+++ docker_build.sh
@@ -6,8 +6,9 @@ cd cesium_web
 docker build -t cesium/web .
 
 # Run tests before uploading
-(cd docker-compose && docker-compose up &)
+docker-compose up &
 echo "Sleeping for 60 seconds while server starts"
 sleep 60
 
+echo -e "\n\nports:\n    app: 9000" >> test_config.yaml
 PYTHONPATH=. xvfb-run -a pytest -v cesium_app/tests/frontend/test_pipeline_sequentially.py
