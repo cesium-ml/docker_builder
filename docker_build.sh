1488b3ba4747f5beb184efa48dc7a635c813dd14
Brett Naul <brettnaul@gmail.com>
2017-10-25 09:37:11 -0700

    Change sleep time

--- docker_build.sh
+++ docker_build.sh
@@ -7,8 +7,9 @@ docker build -t cesium/web .
 
 # Run tests before uploading
 docker-compose up &
-echo "Sleeping for 60 seconds while server starts"
-sleep 60
+# TODO this can be reduced to ~15s once `run_production` is changed to skip `npm install`
+echo "Sleeping for 180 seconds while server starts"
+sleep 180
 
-echo -e "\n\nports:\n    app: 9000" >> test_config.yaml
+echo -e "\nports:\n    app: 9000" >> test_config.yaml
 PYTHONPATH=. xvfb-run -a pytest -v cesium_app/tests/frontend/test_pipeline_sequentially.py
